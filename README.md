6d248146cb16b8a8774c2b3e0a7cb1240cc460db
Habibie Faried <habibiefaried@gmail.com>
2021-05-07 15:01:14 +0700

    Add a whitelist

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tcp-proxy-whitelist
 Simple golang tcp proxy for security purposes (with whitelisting IP)
+# How to use
+
+```
+BIND_PORT=9090 REMOTE_ADDR_PAIR=localhost:8080 WHITELISTED_SUBNET=127.0.21.2/32,172.17.0.1/32 go run main.go
+```
--- main.go
+++ main.go
@@ -5,11 +5,27 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
+	var whitelisted_subarray []*net.IPNet
+
 	if (os.Getenv("BIND_PORT") == "") || (os.Getenv("REMOTE_ADDR_PAIR") == "") {
 		panic("env BIND_PORT and REMOTE_ADDR_PAIR must be set. REMOTE_ADDR_PAIR have this format `<ip:port>`")
+	}
+
+	if os.Getenv("WHITELISTED_SUBNET") == "" {
+		fmt.Println("[WARN] env WHITELISTED_SUBNET is empty, receiving connections from ANY SUBNET")
+	} else {
+		for _, v := range strings.Split(os.Getenv("WHITELISTED_SUBNET"), ",") {
+			_, ipnetB, err := net.ParseCIDR(v)
+			if err != nil {
+				fmt.Printf("error parse %v\n", err)
+			} else {
+				whitelisted_subarray = append(whitelisted_subarray, ipnetB)
+			}
+		}
 	}
 
 	localAddr := "0.0.0.0:" + os.Getenv("BIND_PORT")
@@ -22,13 +38,38 @@ func main() {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Printf("error accepting connection: %v", err)
+			fmt.Printf("error accepting connection: %v\n", err)
+			conn.Close()
 			continue
 		}
+
+		if addr, ok := conn.RemoteAddr().(*net.TCPAddr); ok {
+			IsWhitelisted := false
+
+			fmt.Println("Connected from " + addr.IP.String())
+			for _, v := range whitelisted_subarray {
+				if v.Contains(addr.IP) {
+					IsWhitelisted = true
+					break
+				}
+			}
+
+			if !IsWhitelisted {
+				fmt.Println("Not eligible")
+				conn.Close()
+				continue
+			}
+
+		} else {
+			fmt.Println("cannot cast the remote addr to TCP. Skipping..")
+			conn.Close()
+			continue
+		}
+
 		go func() {
 			conn2, err := net.Dial("tcp", os.Getenv("REMOTE_ADDR_PAIR"))
 			if err != nil {
-				fmt.Printf("error dialing remote addr %v", err)
+				fmt.Printf("error dialing remote addr %v\n", err)
 				return
 			}
 			go io.Copy(conn2, conn)
